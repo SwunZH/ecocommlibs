4cff09ff1e479f5f519f207262a61ee85f543b3a
datakun <me@kimjunu.com>
2018-02-23 16:04:43 +0900

    - 파일 최적화.

--- OpenCV320/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/0.Libraries/opencv320_source/opencv-3.2.0/include
-# Build directory: D:/0.Libraries/opencv320_source/build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
