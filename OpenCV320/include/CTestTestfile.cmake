6f32c7f2fade8f5071178ceac2e7fec532a79bf6
datakun <me@kimjunu.com>
2018-02-23 16:03:28 +0900

    - OpenCV 3.2, VC14, x86 버전 업로드 - 다른 플랫폼의 라이브러리 파일과 맞춰야함. 일단 나중에.

---
+++ OpenCV320/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/0.Libraries/opencv320_source/opencv-3.2.0/include
+# Build directory: D:/0.Libraries/opencv320_source/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
